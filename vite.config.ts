e0932001f270e63bf2a2c878b835d2d83df5a331
ivangithubed <fmdostoevskiy@gmail.com>
2025-11-07 22:21:36 +0200

    minor fix - vercePreset

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { vitePlugin as reactRouter } from "@react-router/dev";
+import { vercelPreset } from "@vercel/react-router/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [
+    reactRouter({
+      presets: [vercelPreset()],
+    }),
+    tsconfigPaths(),
+  ],
 });
